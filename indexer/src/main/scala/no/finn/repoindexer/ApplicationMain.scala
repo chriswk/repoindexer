35dc0941f1c0dfa4b39fef6e2479827146e6f0da
chrkolst <christopher.kolstad@finn.no>
2015-09-07 14:26:00 +0200

    Add scan of git.finn.no

--- indexer/src/main/scala/no/finn/repoindexer/ApplicationMain.scala
+++ indexer/src/main/scala/no/finn/repoindexer/ApplicationMain.scala
@@ -1,16 +1,16 @@
 package no.finn.repoindexer
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, ActorSystem}
 import no.finn.repoindexer.actors.StashProjectsActor
 import com.typesafe.config.ConfigFactory
 
 object ApplicationMain extends App {
 
-  val config = ConfigFactory.load()
+  val config = ConfigFactory.load(ConfigFactory.systemProperties()).withFallback(ConfigFactory.load())
   val system = ActorSystem("RepoIndexer")
   val baseUrl = config.getString("stash.url")
 
-  val projects = system.actorOf(StashProjectsActor.props, "projectsIndexer")
+  val projects = system.actorOf(Props[StashProjectsActor])
 
   projects ! GetProjects(baseUrl)
 
